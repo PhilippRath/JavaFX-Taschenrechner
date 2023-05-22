582756e086e613f4cc00a0f7dfee85c595c59d3f
Philipp <prath@bbw-fi.de>
2023-05-22 21:58:48 +0200

    Projekt erzeugen

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module de.bbwfi.taschenrechner {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens de.bbwfi.taschenrechner to javafx.fxml;
+    exports de.bbwfi.taschenrechner;
+}
---
+++ src/test/java/de/bbwfi/taschenrechner/HelloApplicationTest.java
@@ -0,0 +1,12 @@
+package de.bbwfi.taschenrechner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @Test
+    void start() {
+    }
+}
