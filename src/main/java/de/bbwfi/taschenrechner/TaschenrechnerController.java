2df81953225ae24eba70dc3f3b14f7090b3d9653
ljm.koehnen-garcon <ljm.koehnen-garcon@bbw-fi.de>
2023-05-24 12:09:24 +0200

    buttons labelo

--- src/main/java/de/bbwfi/taschenrechner/TaschenrechnerController.java
+++ src/main/java/de/bbwfi/taschenrechner/TaschenrechnerController.java
@@ -3,16 +3,25 @@ package de.bbwfi.taschenrechner;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class TaschenrechnerController {
+
+public abstract class TaschenrechnerController {
+    private Taschenrechner taschenrechner;
 
     @FXML
     private Label displayLabel;
-
+    public void initialize(){
+        taschenrechner = new Taschenrechner();
+    };
+    protected  void onZeroButtonPress() {
+        String displayText = displayLabel.getText();
+        if (!displayText.equals("0")) {
+            displayLabel.setText(displayText + "0");
+        }
+    }
     @FXML
     protected void onIstgleichButtonPress() {
         String displayText = displayLabel.getText();
 
         displayLabel.setText(displayText + "9");
-
     }
 }
