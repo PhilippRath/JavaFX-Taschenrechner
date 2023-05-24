ef71302a9ae97db637f5cae164ecae11e3128944
Philipp <prath@bbw-fi.de>
2023-05-24 10:32:57 +0200

    Änderungen

--- src/main/java/de/bbwfi/taschenrechner/TaschenrechnerController.java
+++ src/main/java/de/bbwfi/taschenrechner/TaschenrechnerController.java
@@ -4,11 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class TaschenrechnerController {
+
     @FXML
     private Label displayLabel;
 
     @FXML
     protected void onIstgleichButtonPress() {
-        displayLabel.setText("999");
+        String displayText = displayLabel.getText();
+
+        displayLabel.setText(displayText + "9");
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module de.bbwfi.taschenrechner {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens de.bbwfi.taschenrechner to javafx.fxml;
     exports de.bbwfi.taschenrechner;
 }
